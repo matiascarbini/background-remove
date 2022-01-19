218bfbae662f8dea958ee2da147c5f7a19b76f6a
Matias Carbini <matias.carbini@gmail.com>
2022-01-19 07:29:15 -0300

    mejoras

--- functions/backgroundRemove.py
+++ functions/backgroundRemove.py
@@ -1,6 +1,7 @@
 from rembg.bg import remove
 import numpy as np
 import io
+import urllib.request
 from PIL import Image, ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
@@ -11,9 +12,12 @@ backgroundRemove_api = Blueprint('backgroundRemove_api', __name__)
 @backgroundRemove_api.route('/background-remove', methods=["POST"])
 def backgroundRemove():    
   url_image = request.json["image"]
-  
-  import urllib
-  urllib.request.urlretrieve(url_image, "tmp/input.png")
+    
+  opener=urllib.request.build_opener()
+  opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+  urllib.request.install_opener(opener)
+
+  urllib.request.urlretrieve(url_image, "tmp/input.png")  
   
   input_image = 'tmp/input.png'
   output_image = 'tmp/output.png'
